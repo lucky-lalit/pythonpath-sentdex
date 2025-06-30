cbb7d55d394ffe532fcb263ccf13bfe2a5df805d
lalit tiwari <ltiwari2005@gmail.com>
2025-06-30 21:18:54 +0530

    finished w.4.t variable,datatype,tuple,for.

--- test/test.py
+++ test/test.py
@@ -1,1 +1,13 @@
+def lalit():
+    _a1=2
+    programming_language='python',"c",5
+    print(type(_a1))
+    print(programming_language)
+    print(type(programming_language))
+    for lucky in programming_language:
+        print(lucky)
+        print(type(lucky))
+
 print("Hello World")
+lalit()
+# lalit()
