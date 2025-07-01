971980c160c96c8c9e0bd44bab4af0d7c68988ce
lalit tiwari <ltiwari2005@gmail.com>
2025-07-01 20:01:55 +0530

    given coordiantes

---
+++ ch3.py
@@ -0,0 +1,10 @@
+game = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+# game[1] = 100
+#print(type(game))
+# print(game)
+for row in game:
+    print(row)
+    
---
+++ ch4.py
@@ -0,0 +1,10 @@
+game = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+print("   0  1  2")
+#count=0
+for count,row in enumerate(game):
+    print(count,row)
+    # print(type(count),count)
+    # count=count+1
