d657715cf322c36e084b3de7b2cb727709f11ebc
lalit tiwari <ltiwari2005@gmail.com>
2025-07-01 21:58:30 +0530

    learned about indexes & slice

---
+++ ch5.py
@@ -0,0 +1,15 @@
+game = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+print("   0  1  2")
+game[2][0] = 1
+game[1][2] = 2
+for count,row in enumerate(game):
+    print(count,row)
+    
+'''_1=[1,2,3,4,5,6]
+_1[-1] = "a"
+_1[-3]="b"
+_1[-4]="r"
+print(_1[:])'''
